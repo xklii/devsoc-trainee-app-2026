a9ab6dd41994c9466f32c54142efcf4e74637396
Brigette <z5587785@ad.unsw.edu.au>
2026-02-14 16:29:36 +1100

    woww i rushed everything. anyways finished light/dark mode (stole someone elses code for persistance in local storage

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const toggleBtn = document.getElementById('toggle-mode');
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+const toggleBtn = document.getElementById("toggle-mode");
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+  document.body.setAttribute("data-theme", savedTheme);
+} else {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (prefersDark) {
+    document.body.setAttribute("data-theme", "dark");
+  }
+}
+toggleBtn.addEventListener("click", () => {
+  const currentTheme = document.body.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  document.body.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
 });
